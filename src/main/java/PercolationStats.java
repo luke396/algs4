73d5105ed23cceaf22d4ef0f93668fcabffa5474
luke <lukke@qq.com>
2022-04-30 12:28:47 +0800

    72

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -11,31 +11,25 @@ public class PercolationStats {
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         perOfOpen = new double[trials];
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException("n and trials must be greater than 0");
-        }
 
-        Percolation perc = new Percolation(n);
-
-        for (int i = 0; i < trials; i++) {
-            for (int j = 0; j < Math.pow(n, 2); j++) {
+        for (int i = 0; i < trials; i++) { // i to control the number of trials
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
                 int row = StdRandom.uniform(1, n + 1); // random location uniformly [1, n+1)
                 int col = StdRandom.uniform(1, n + 1);
                 if (!perc.isOpen(row, col)) {
                     perc.open(row, col);
-                } // 不要在for循环内改变控制循环的变量
-                if (perc.percolates()) { // when percolating, record the num.
-                    perOfOpen[i] = perc.numberOfOpenSites() / Math.pow(n, 2);
-                    break;
                 }
             }
+            // when percolating, record the num.
+            perOfOpen[i] = (double) perc.numberOfOpenSites() / (n * n);
+            // to ensure the percentage is a double, or the '/' with tow int will be int
         }
     }
 
 
     // sample mean of percolation threshold
     public double mean() {
-        // x is the fraction fo the open site
         return StdStats.mean(perOfOpen);
     }
 
@@ -60,11 +54,12 @@ public class PercolationStats {
 
     // test client (see below)
     public static void main(String[] args) {
-        if (StdIn.isEmpty()) {
-            StdOut.println("Usage: java PercolationStats <n> <trials>");
-        } else {
+        if (!StdIn.isEmpty()) {
             int n = StdIn.readInt();
             int t = StdIn.readInt();
+            if (n <= 0 || t <= 0) {
+                throw new IllegalArgumentException("n and t must be greater than 0");
+            }
             PercolationStats test = new PercolationStats(n, t);
             StdOut.printf("mean=%.5f\n", test.mean());
             StdOut.printf("stddev=%.5f\n", test.stddev());
