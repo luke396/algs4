1f18cf3e4f6f925075639f050b98ef7b7a9a3c43
luke <lukke@qq.com>
2022-06-06 13:40:53 +0800

    feat: preliminary complete RandomizedQueue.java

--- src/main/java/queues/Permutation.java
+++ src/main/java/queues/Permutation.java
@@ -1,4 +1,12 @@
 package queues;
 
+import com.google.inject.internal.util.Strings;
+import edu.princeton.cs.algs4.StdIn;
+
+// Write a client program Permutation.java that takes an integer k as a command-line argument;
+// reads a sequence of strings from standard input using "StdIn.readString()";
+// and prints exactly k of them, uniformly at random. Print each item from the sequence at most once.
 public class Permutation {
-        }
+    RandomizedQueue<Strings> randomizedQueue = new RandomizedQueue<>();
+    while (!StdIn.isEmpty())
+}
--- src/main/java/queues/RandomizedQueue.java
+++ src/main/java/queues/RandomizedQueue.java
@@ -1,4 +1,103 @@
 package queues;
 
-public class RandomizedQueue {
+// A randomized queue is similar to a stack or queue,
+// except that the item removed is chosen uniformly at random among items in the data structure.
+
+// This will implement by linked list, just for practice, not consider the question requirements.
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int count;
+    private static final int INIT_CAPACITY = 8;
+    private Item[] a;
+
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        a = (Item[]) new Object[INIT_CAPACITY];
+        count = 0;
+    }
+
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+        return count;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null) throw new IllegalArgumentException("Enqueue item can't be null!");
+        a[count++] = item;
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("randomized queue is empty!");
+        int index = StdRandom.uniform(count);
+        Item out = a[index];
+        a[index] = null;
+        return out;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+        return a[StdRandom.uniform(count)];
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator();
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        int nextCount;
+        Item[] out = (Item[]) new Object[count]; // collect has outed
+
+        @Override
+        public boolean hasNext() {
+            return nextCount != count;
         }
+
+        @Override
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException("No Next!");
+            boolean notFind = true;
+
+            while (notFind) {
+                Item willOut = sample();
+                for (Item temp : out) {
+                    if (willOut == temp) {
+                        willOut = null;
+                        break;
+                    } else {
+                        out[nextCount++] = willOut;
+                        return willOut;
+                    }
+                }
+                if (willOut != null) notFind = false;
+            }
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        StdOut.println("Wish me luck!");
+    }
+}
